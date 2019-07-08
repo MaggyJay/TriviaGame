3f7ed99eed63b42596ca44a08520c8056b9910df
Magdalene <magdalene809@gmail.com>
2019-07-08 06:01:18 -0500

    Making some adjustments to the timer, need to get seconds right

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,61 @@
 // timer js 
 
-var number= 600;
+$(document).ready(function () {
+    var number = 600;
+    var intervalID;
 
-$('#start-button').on('click', run){
+    $('#finished-quiz').hide();
+
+
+    $('#start-button').on('click', function () {
+        run();
+    });
 
     function run() {
         clearInterval(intervalID);
         intervalID = setInterval(decrement, 600);
+
+
     }
-}
 
-function decrement () {
-    number --;
 
-    $('#timer').append(number);
-}
+    function decrement() {
+        number--;
 
-if (number === 0) {
-    stop ();
+        
 
-    $('#sim-quiz').hide();
-    $('#finished-quiz').show();
-}
+        // $('#timer').append(number);
+        $('#timer').html('<p> Time Remaining: ' + number + '</p>');
+        if (number === 0) {
+            // stop();
+            clearInterval(intervalID);
+            alert('Timer works!');
+    
+            $('#sim-quiz').hide();
+            $('#finished-quiz').show();
+        }
+    }
 
-function stop (){
-    clearInterval(IntervalID);
-}
+    // function timeConverter(t) {
 
-run();
+    //     var minutes = Math.floor(t / 60);
+    //     var number = t - (minutes * 60);
+
+    // }
+
+    // if (number === 0) {
+    //     // stop();
+    //     clearInterval(intervalID);
+    //     alert('Timer works!');
+
+    //     $('#sim-quiz').hide();
+    //     $('#finished-quiz').show();
+    // }
+
+    // function stop() {
+    //     clearInterval(intervalID);
+    // }
+
+    //run();
+
+})
