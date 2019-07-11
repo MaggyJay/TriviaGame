40395708ee6317b15e0f7b849c76a7338553f8e0
Magdalene <magdalene809@gmail.com>
2019-07-11 04:28:37 -0500

    Working on code for checking answers in JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,28 +18,54 @@ $(document).ready(function () {
 
             number -= 1;
             clearInterval(intervalID);
-    
+
             if (number <= 0) {
-    
+
                 clearInterval(timer);
                 alert('Timer works!');
-    
+
                 $('#sim-quiz').hide();
                 $('#finished-quiz').show();
+                $('#finished-quiz').html('<p> Correct Answers: ' + score);
             }
-    
-    
-    
-    
-    
+
+
+
+
+
             $('#timer').append(number);
             $('#timer').html('<p> Time Remaining: ' + number + '</p>');
-    
+
         }, 1000);
+
+        // $('#submit-button').on('click', function () {
+        //     $('#sim-quiz').hide();
+        //     $('#finished-quiz').show();
+        //     $('#finished-quiz').html('<p> Correct Answers: ' + score);
     
+        // })
+
     })
-    });
+
+  
+
+    var answers = ["B", "D", "B", "E", "C", "C", "E", "A", "D"];
+
+    total = answers.length;
+
+    function checkAnswer(radioName) {
+        var radios = document.getElementById(radioName);
+        for (var y = 0; y < radios.length; y++)
+            if (radios[y].checked) return radios[y].value;
+    }
+
+    function getScore() {
+        var score = 0;
+        for (var i = 0; i < total; i++)
+            if (checkAnswer("question" + i) === answers[i]) score += 1;
+        return score;
+    }
+})
 
 
-
-   
+    // });
